b8b7f7ea5426f0580337477d0055f1ccbc7ffd65
Sébastien Fournier <sebastien.fournier.95@gmail.com>
2025-10-28 08:17:17 +0100

    go on feature put the count deces function

--- run-local-ES-update.sh
+++ run-local-ES-update.sh
@@ -2,11 +2,15 @@
 
 sudo apt-get update -y && sudo apt-get install make
 
+make clean
 make config
+
+# Copier le backup dans backend
+cp -r ../backup/upload ./backend/upload
 
 make datagouv-to-upload
 
 make recipe-run
 make watch-run
 
-make backup
+#make backup
